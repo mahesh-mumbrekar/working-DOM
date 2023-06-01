ae4889ec018e2320a5b6d9430359db39e578f656
kwingston <mahesh.mummbrekar@gmail.com>
2023-06-01 20:50:25 +0530

    added some other javascript properties

--- app.js
+++ app.js
@@ -6,3 +6,18 @@ anchorElement.href = 'http://google.com'
 
 let anchorElement2 = document.querySelector('#external-link-2');
 anchorElement2.href = "http://youtube.com"
+
+
+let h1Element = documnet.body.firstElementChild;
+console.dir(h1Element);
+
+
+/* if u want to access the parent then*/
+
+console.dir(h1Element.parentElement);
+
+
+/* if u want to access sibling then*/
+
+console.dir(h1Element.nextElementSibling);
+
