93b982b279a56ce5607dacaf80b4fe162fb23fd4
kwingston <mahesh.mummbrekar@gmail.com>
2023-06-04 22:13:15 +0530

    listening to user input by eventlistner

--- new.js
+++ new.js
@@ -1,8 +1,30 @@
+/*added eventlistner*/
+
+
 let newParagraph = document.querySelector('p');
 
 function changeparagraph() {
-    newParagraph.textContent = 'You clicked!'
+    newParagraph.textContent = 'You clicked!';
 }
 
+newParagraph.addEventListener('click', changeparagraph);
 
-newParagraph.addEventListener('click', changeparagraph )
+
+/* listening to userinput */
+
+
+let inputElement = document.querySelector('input');
+
+function retriveUserInput() {
+    let enteredText = inputElement.value;
+    console.log(enteredText)
+
+}
+/*or alternate fuction for this having same output("event" object)
+function retriveUserInput(event) {
+    let enteredText = event.target.value;
+    console.log(enteredText)
+*/
+
+
+inputElement.addEventListener('input', retriveUserInput);
