eb098863a9807c4c06e6e13262b8e48fc274b00a
kwingston <mahesh.mummbrekar@gmail.com>
2023-06-05 18:44:30 +0530

    add and remove class name using classList method

--- task.js
+++ task.js
@@ -24,9 +24,17 @@ remainingCharElement.style.color = "red"
 
 /* to change class name of some element or add class name */
 /* 1) add class name */
+/* in this method the new class is overwritten on old class if alredy present*/
 
 let buttonElement = document.querySelector('button')
 buttonElement.className = 'push'
+
+
+/* 2) add new class and remove class in element */
+/* classList is use to add or remove classes without changing old classes*/
+
+buttonElement.classList.add('pull')
+buttonElement.classList.remove('push')
 
 
 
