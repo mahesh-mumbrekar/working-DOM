5a2d552de59d8b63c27f13c47ff3c505e2ce0579
kwingston <mahesh.mummbrekar@gmail.com>
2023-06-07 23:17:25 +0530

    for ....of loop example

--- loops.js
+++ loops.js
@@ -1,3 +1,16 @@
 for (let i = 0; i < 10; i++){
     console.log(i)
 }
+
+/* for....of loops used in array */
+
+
+
+let commonActivities = ['play', 'code', 'read']
+
+for (let Activities of commonActivities) {
+    console.log(Activities)
+}
+
+
+
